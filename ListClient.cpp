a69658af4cc46ab0a770b01fdf8857ba0d4a3ea7
Amon9663 <Amon9663@users.noreply.github.com>
2017-03-15 16:42:15 -0400

    ListClient.cpp

--- ListClient.cpp
+++ ListClient.cpp
@@ -10,5 +10,12 @@ int main()
 
  //Do some stuff with L1, L2, ...
  // ...
-
+L1.insert(8,1);
+L1.insert(6,1);
+L1.insert(5,2);
+L2.insert(54,1);
+L2.insert(27,1);
+L2.insert(45,2);
+L1.display();
+L2.display();
 }
